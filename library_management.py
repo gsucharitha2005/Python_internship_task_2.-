dc422db20d412bde04b20bea1dac4ad8c0fd3531
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-06-08 22:30:09 +0530

    upadted

--- library_management.py
+++ library_management.py
@@ -1,23 +1,26 @@
 library=[]
+
 def add_book():
-	n=int(input("no.of books:"))
-	for i in range(n):
-        	book_ISBN=int(input("enter the ISBN:"))
-        	book_name=input("enter the book name:")
-        	book_author=input("enter the book author:")
-        	book={
-            		"book_ISBN":book_ISBN,
-            		"book_name":book_name,
-            		"book_author":book_author
-        		}
-        	library.append(book)
-	print("___________________________________________________________")
-	print("book_ISBN\tbook_name\tbook_author")
-	print("____________________________________________________________")
-	for i in library:
-		print(i["book_ISBN"],"\t\t",i["book_name"],"\t\t",i["book_author"])
-	print("books added successfully")
-	
+    n=int(input("no.of books:"))
+    for i in range(n):
+        book_ISBN=int(input("enter the ISBN:"))
+        book_name=input("enter the book name:")
+        book_author=input("enter the book author:")
+        book={
+            "book_ISBN":book_ISBN,
+            "book_name":book_name,
+            "book_author":book_author
+        }
+        library.append(book)
+
+    print("___________________________________________________________")
+    print("book_ISBN\tbook_name\tbook_author")
+    print("____________________________________________________________")
+    for i in library:
+        print(i["book_ISBN"],"\t\t",i["book_name"],"\t\t",i["book_author"])
+    print("books added successfully")
+
+
 def issue_book():
     issue_ISBN=int(input("Enter ISBN to issue: "))
     for i in library:
@@ -27,49 +30,51 @@ def issue_book():
             break
     else:
         print("book not found")
+        return
+
+    print("\nRemaining Books in Library:")
+    print("___________________________________________________________")
+    print("book_ISBN\tbook_name\tbook_author")
+    print("____________________________________________________________")
+    for i in library:
+        print(i["book_ISBN"],"\t\t",i["book_name"],"\t\t",i["book_author"])
+
 
 def return_book():
     return_ISBN=int(input("Enter ISBN to return: "))
     for i in library:
         if i["book_ISBN"]==return_ISBN:
-            library.pop()
-            print("book return successfully")
+            print("book already exists in library")
             break
     else:
-        print("book not found")
-         
+        print("book not found (cannot return)")
+        
+
 def calculate_fine_days():
-	fine_per_day=5
-	late_days=int(input("enter late days:"))
-	fine=late_days * 5
-	print(fine)
+    fine_per_day=5
+    late_days=int(input("enter late days:"))
+    fine=late_days * 5
+    print(fine)
+
+
 def menu():
-	while True:
-		print("\n___________________________Library Management_________________________")
-		option=int(input("1.add_book\n2.issue_book\n3.return_book\n4.calculate_fine_days\n5.exist\n"))
+    while True:
+        print("\n___________________________Library Management_________________________")
+        option=int(input("1.add_book\n2.issue_book\n3.return_book\n4.calculate_fine_days\n5.exist\n"))
 
-		if option==1:
-        		add_book()
-		elif option==2:
-        		issue_book()
-		elif option==3:
-        		return_book()
-		elif option==4:
-        		calculate_fine_days()
-		elif option==5:
-			print("exiting library")
-			break
-		else:
-            		print("invalid option")
-	
-                        
+        if option==1:
+            add_book()
+        elif option==2:
+            issue_book()
+        elif option==3:
+            return_book()
+        elif option==4:
+            calculate_fine_days()
+        elif option==5:
+            print("exiting library")
+            break
+        else:
+            print("invalid option")
+
+
 menu()
-
-	
-	
-	
-
-
-
-
-
